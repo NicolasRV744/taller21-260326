ff48e11636bc9c3fdd1478d3ce420189ea931560
NicolasRV744 <est.nicolas.romerov@unimilitar.edu.co>
2026-03-26 10:05:30 -0500

    Agregue la estructura de nodo y lista enlazada pero me sale un error porque no he hecho la integracion de la misma

---
+++ taller21260326/src/main/java/com/mycompany/taller21260326/Taller21260326.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Nodo {
+    int valor;
+    Nodo siguiente;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+    }
+}
+
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        
+    }
+}
