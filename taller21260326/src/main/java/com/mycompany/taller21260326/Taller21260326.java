9be4225a422dc6022e3a5d6ab74cc92c7abd96fb
NicolasRV744 <est.nicolas.romerov@unimilitar.edu.co>
2026-03-30 22:14:30 -0500

    Tuve que hacer un cambio a la hora de ejecutar, busque el error por chat y me indico que cambiara el main class a la hora de hacer run y cambiar al paquete que tiene el programa

--- taller21260326/src/main/java/com/mycompany/taller21260326/Taller21260326.java
+++ taller21260326/src/main/java/com/mycompany/taller21260326/Taller21260326.java
@@ -39,7 +39,7 @@ class ListaEnlazada {
     return suma;
  }
 }
-public class MissingNumberLista {
+class MissingNumberLista {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
